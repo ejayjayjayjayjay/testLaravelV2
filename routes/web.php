41d775d5182f9d85d725213822639ecf8cfe6e9b
Josell Vibar <vibarjosell@gmail.com>
2022-11-12 17:40:39 +0800

    Update web.php and create post controller

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+route::get('/p',[App\Http\Controllers\ProfilesController::class, 'create']);
+
 Route::get('profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
 
@@ -28,5 +30,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
  */
+
+route::get('/p',[App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
  
