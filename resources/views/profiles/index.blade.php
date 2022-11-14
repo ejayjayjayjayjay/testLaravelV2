13e11ae6066eb6c8b5147dc742cd6b7b5e574079
Josell Vibar <vibarjosell@gmail.com>
2022-11-14 19:53:17 +0800

    make updates on front-end for the /p/create path

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -2,23 +2,6 @@
 
 @section('content')
 <div class="container">
-    <!-- <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div> -->
     <div class="row">
         <div class="col-3 p-5">
             <img src="/images/ejay.png" class="rounded-circle" style="height: 200px; width: 200px;" alt="DogPhoto">           
